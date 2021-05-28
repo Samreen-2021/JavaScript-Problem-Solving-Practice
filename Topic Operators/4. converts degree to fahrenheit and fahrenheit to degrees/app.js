0ccc3d42e9ba512ac274cf4b3593a9676e1c6924
samreen-2021 <determinedlearner2019@gmail.com>
2021-05-28 13:38:04 +0500

    Implentation and Practice of javascript in Problem solving assignment

--- Topic Operators/4. converts degree to fahrenheit and fahrenheit to degrees/app.js
+++ Topic Operators/4. converts degree to fahrenheit and fahrenheit to degrees/app.js
@@ -1,23 +1,19 @@
 // Write a program that converts degree to fahrenheit and fahrenheit to degrees.
 
 
-let c= 10;
-let f = (1.8 * c) + 32; 
-console.log(f);
-// when temp is 20 degree celcius
+// let c= 10;
+// let f = (1.8 * c) + 32; 
+// console.log(f);
+// // when temp is 20 degree celcius
 
-c = 20;
-f = (1.8 * c) + 32; 
-console.log(f);
-
-// c= 0;
+// c = 20;
 // f = (1.8 * c) + 32; 
 // console.log(f);
 
-// let x =4;
-// let y= (2 *x) + 5;
-// console.log(y);
+function tempConvert(){
 
-// x=6;
-// console.log(y);
-
+let celciusTemp = document.querySelector('input').value;
+let C = parseInt(celciusTemp);
+let F= (1.8 * C) + 32;
+alert("Temperature in Farenheit is " + (F));
+}
