8994e4425b5185da9f880886c3add51ea69e9862
samreen-2021 <determinedlearner2019@gmail.com>
2021-06-01 10:42:06 +0500

    Implementation and practice of basic concept of javascript in problem-splving task completed

---
+++ Datatypes/checkIt/app.js
@@ -0,0 +1,29 @@
+// Assign an integer value to your variable.
+let x = 5;
+console.log(typeof x);              //type  is number
+
+// Assign an decimel value to your variable.
+let y= 5.6;
+console.log(typeof y);              // type is number
+
+// Assign an boolean value to your variable.
+let z= true;
+console.log(typeof z);              // type is booleam
+
+// Assign any string to your variable. Remember to put quotation marks. Check its data type from the console.
+ //  Assign your variable a value of null. Check its type in the console.
+
+ let f = null ;
+ console.log( typeof f);              // type is object
+
+let s = "apples";
+console.log(typeof s);
+// Now remove the quotation marks and see if it is still a string or do you see an error?
+ let k= apples;
+ console.log(typeof k);        //Uncaught ReferenceError: apples is not    defined at app.js:20    
+
+
+
+
+
+
